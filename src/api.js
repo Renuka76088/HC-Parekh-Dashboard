aef0e11f3d390c5f4d0d03cd0592af36ce3c6757
SumitSahu444 <sumitkumarsahu141@gmail.com>
2026-05-07 18:40:07 +0530

    live api fix

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://ap.hcparekh.com/api/';
+const API_BASE_URL = 'https://api.hcparekh.com/api/';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
